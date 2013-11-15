b41bb51130726e3fe9cd717ed0ce8b40ebf5b8e8
Andrew Kimn <akimn89@gmail.com>
2013-11-14 16:10:50 -0800

    Another commit

--- app/models/listing.rb
+++
@@ -1,9 +0,0 @@
-def self.search(search)
-    s =  "%#{search}%"
-    if search
-      find(:all, :conditions => ["comments LIKE ? or doctor LIKE ? or website LIKE ? or     
-      url LIKE ? or date LIKE ?", s, s, s, s, s])
-    else
-      nil
-    end
-  end
